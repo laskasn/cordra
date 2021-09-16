549f31a0eeff1d8de0eab1d690e5f8f05e1caced
laskasn <laskarisn@grnet.gr>
2021-09-17 01:28:40 +0300

    Purifying url base64 encoded strings on javascript

--- src/main/webapp/js/cordra/CordraApp.js
+++ src/main/webapp/js/cordra/CordraApp.js
@@ -183,7 +183,7 @@
           let storedOptions = getStoredCordraOptions();
 
           if(storedOptions.token!=null && storedOptions.keycloakConfig!=null){
-            var parsedToken = JSON.parse(atob(storedOptions.token.split('.')[1]));
+            var parsedToken = JSON.parse(atob(purifyEncoded(storedOptions.token.split('.')[1])));
             response['name'] = parsedToken.name;
             response['username'] = parsedToken.preferred_username;
             response['userId'] = parsedToken.sub;
@@ -194,6 +194,22 @@
         }
 
 
+        function purifyEncoded(input) {
+          // Replace non-url compatible chars with base64 standard chars
+          input = input
+              .replace(/-/g, '+')
+              .replace(/_/g, '/');
+
+          // Pad out with standard base64 required padding characters
+          var pad = input.length % 4;
+          if(pad) {
+            if(pad === 1) {
+              throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+            }
+            input += new Array(5-pad).join('=');
+          }
+          return input;
+        }
 
 
         function onGotInitData(response) {
